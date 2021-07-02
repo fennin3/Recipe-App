cda3fb8818fc17939869a5c859e680fdb2d93212
Francis Ennin <francisennin@Franciss-MacBook-Pro.local>
2021-07-02 23:21:40 +0000

    make it better

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import django_heroku
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -184,6 +184,9 @@ EMAIL_HOST_PASSWORD = 'xtioscgwkfkvqgpf'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
