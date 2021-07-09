6e75ed6797c46bcb8a087ae98432d6921614c956
Francis Ennin <enninfrancis47@gmail.com>
2021-07-09 07:45:03 +0000

    added whitenoise

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6^4ca9$ml4l1c#36gkc*z3#8(h_(9efte_*(03byw4real%7rl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["resipe.herokuapp.com", "localhost", "www.resipe.herokuapp.com"]
 
