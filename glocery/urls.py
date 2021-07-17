7179b795937fe9bdd5bd31b12c10600a48f9ca46
Francis Ennin <enninfrancis47@gmail.com>
2021-07-17 09:55:46 +0000

    changed gloceries to groceries

--- glocery/urls.py
+++ glocery/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("get-all-categories/", views.GetAllCategories.as_view(), name="all_g_cat"),
-    path("get-category-gloceries/<id>/", views.GetCategoryGloceries.as_view(),name="cat_g_recipes"),
-    path("get-all-gloceries/", views.GetAllGloceries.as_view(), name="allglo"),
-    path("glocery-detail/<id>/", views.GetGloceryDetail.as_view(), name="glo_detail"),
-    path("save-glocery/", views.SaveGlocery.as_view(), name="save_glo"),
-    path("get-saved-gloceries/<email>/", views.GetSavedGlocery.as_view(), name="get_g_sr"),
-    path("remove-saved-glocery/<id>/", views.RemoveSavedGlocery.as_view(), name="remove_g_sr")
+    path("get-category-groceries/<id>/", views.GetCategoryGloceries.as_view(),name="cat_g_recipes"),
+    path("get-all-groceries/", views.GetAllGloceries.as_view(), name="allglo"),
+    path("grocery-detail/<id>/", views.GetGloceryDetail.as_view(), name="glo_detail"),
+    path("save-grocery/", views.SaveGlocery.as_view(), name="save_glo"),
+    path("get-saved-groceries/<email>/", views.GetSavedGlocery.as_view(), name="get_g_sr"),
+    path("remove-saved-grocery/<id>/", views.RemoveSavedGlocery.as_view(), name="remove_g_sr")
 ]
